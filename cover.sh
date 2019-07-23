eeea72c315daf1a63819f2f77efdbeb28b8281fd
Ruslan Koptiev <rkoptev70@gmail.com>
2019-07-23 23:08:15 +0300

    Code coverage stat added

---
+++ cover.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Set required environment variables
+export NODE_ENV="test"
+# MongoDB database URI
+export MONGODB_URI="mongodb://localhost:27017/crowdfunding-test"
+
+istanbul cover _mocha ./test/**/*_test.js
