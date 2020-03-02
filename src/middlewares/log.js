a1419d26aa046abba9d16db8ff3dfb465daed1e1
kboyko <kboyko1999@gmail.com>
2020-03-02 17:39:28 +0200

    feat: add logging of request body

--- src/middlewares/log.js
+++ src/middlewares/log.js
@@ -1,7 +1,7 @@
 const logger = require('../log');
 
 module.exports = async function (req, res, next) {
-    logger.info(`[request] ${req.ip} ${req.method} ${req.path}`);
+    logger.info(`[request] ${req.ip} ${req.method} ${req.path} ${JSON.stringify(req.body)}`);
 
     next();
 };
