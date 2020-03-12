c19def9b5a22dd4eccaa08bd269fd9c1496064ba
kboyko <kboyko1999@gmail.com>
2020-03-13 00:34:01 +0200

    fix: missing import

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+const logger = require('./log');
+
 module.exports.isValidProjectId = function (projectId) {
     // Must be a 24-digit hex string
     return typeof projectId == 'string' && /^[0-9A-F]{24}$/i.test(projectId);
