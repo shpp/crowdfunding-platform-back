bd08684f99923f44efcebc75f23f4677928cbb41
kboyko <kboyko1999@gmail.com>
2020-03-17 23:47:18 +0200

    WIP: feat: donate without project

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const uuidv4 = require('uuid/v4');
+
 const logger = require('./log');
 const {projects, transactions} = require('./controllers');
 const utils = require('./utils');
-const bodyParser = require('body-parser');
+const liqpayClient = require('../liqpay_client');
 
 // First of all - check environment variables
 if (!utils.isValidEnvironment(process.env)) {
@@ -21,6 +24,27 @@ app.use(require('./middlewares/log'));
 // Set up controllers
 app.use('/api/v1/projects', projects);
 app.use('/api/v1/transactions', transactions);
+app.use('/api/v1/donate', function(req, res) {
+    if (req.body.subscribe === undefined) {
+        utils.sendResponse(res, 400, {error: 'Missing subscription status'});
+        return;
+    }
+    // Validate amount
+    if (!utils.isValidAmount(Number(req.body.amount))) {
+        utils.sendResponse(res, 400, {error: 'Missing or wrong amount.'});
+        return;
+    }
+    const button = liqpayClient.cnb_form({
+        'action': req.body.subscribe ? 'subscribe' : 'pay',
+        'amount': req.body.amount,
+        'currency': 'UAH',
+        'description': 'Благодійний внесок на діяльність ГО',
+        'order_id': uuidv4(),
+        'version': '3',
+        'server_url': process.env.SERVER_URL + '/api/v1/transactions/liqpay-confirmation'
+    });
+    utils.sendResponse(res, 200, {button});
+});
 
 // Set up static route for images
 app.use('/static', express.static(process.env.FILE_STORAGE_PATH || 'uploads'));
