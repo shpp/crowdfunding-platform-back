e99866d6abb9a4ad46a8063253652e4e9af56cb4
kboyko <kboyko1999@gmail.com>
2020-04-23 18:22:37 +0300

    chore: one more try with migrations

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,4 +1,5 @@
 export MONGODB_URI="mongodb://admin:1234@localhost:27017/shpp-crowd-funding"
-rm -rf /state/migrations/
-cp -R /project/src/migrations /state/migrations
-cd /state && migrate
+cp /state/.migrate /state/.migrate.backup
+cp /state/.migrate /project/src/.migrate
+cd /project/src && migrate
+cp /project/src/.migrate /state/.migrate
